171e55e87b6a5d2c5971d6b70054d9a6519552c9
Tanuj Pratap Singh <tanujsinghthakur9999@gmail.com>
2026-05-27 21:03:54 +0530

    Backend of SpendWise

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty(
+                "MONGO_URI",
+                dotenv.get("MONGO_URI")
+        );
+
+        SpringApplication.run(Application.class, args);
     }
 }
